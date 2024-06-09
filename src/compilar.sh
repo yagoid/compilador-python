1af936338ca76262e1ff7ab11f6ec28f4fc32a31
Angel Esquinas <anllelonair@gmail.com>
2024-06-09 19:31:50 +0200

    Cambios minimos

--- src/compilar.sh
+++ src/compilar.sh
@@ -1,7 +1,7 @@
 bison -d -v gramatica_python.y        
-# flex -o latino.lex.c lexico_latino.flex 
+
 flex -o python.lex.c lexico_python.flex 
 
 gcc -o COMPILADO gramatica_python.tab.c python.lex.c
 
-./COMPILADO ./codigo.latino
+./COMPILADO ./test.py
