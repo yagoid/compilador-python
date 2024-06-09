d08ef8de11b4b363144145633df4a311bbea2ab6
llughh <llughprogramacion@gmail.com>
2024-06-09 18:07:56 +0200

    fix sh file

--- src/compilar.sh
+++ src/compilar.sh
@@ -1,5 +1,7 @@
 bison -d -v gramatica_python.y        
 # flex -o latino.lex.c lexico_latino.flex 
-flex -o lexico_python.flex 
-# gcc -o COMPILADO gramatica_latino.tab.c latino.lex.c
+flex -o python.lex.c lexico_python.flex 
+
+gcc -o COMPILADO gramatica_python.tab.c python.lex.c
+
 ./COMPILADO ./codigo.latino
